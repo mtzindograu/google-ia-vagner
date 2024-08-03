6b6c5c93365b025b98560393b29c364a7aa5cf62
mtzindograu <117742961+mtzindograu@users.noreply.github.com>
2024-08-03 11:42:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const respostaDiv = document.querySelector("#resposta-da-ia");
+const textarea = document.querySelector("textarea");
+const submit = document.querySelector("input[type=submit]");
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const API_KEY = "AIzaSyDw7wzUU8n-LxAeofoUL1DMqSRFQNIejjQ";
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+const chat = model.startChat();
+
+submit.addEventListener("click", async () => {
+  if(textarea.value.trim().length == 0) {
+    return;
+  }
+  console.log("foi")
+  const result = await chat.sendMessage(textarea.value);
+  const response = await result.response;
+  const text = response.text();
+  respostaDiv.innerHTML = markdown.toHTML(text);
+})
