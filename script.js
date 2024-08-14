7e00283f6ffebe83b44c107aaa49fc9b7f03beb7
mtzindograu <117742961+mtzindograu@users.noreply.github.com>
2024-08-13 23:37:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,16 +8,14 @@ const API_KEY = "AIzaSyDw7wzUU8n-LxAeofoUL1DMqSRFQNIejjQ";
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-const chat = model.startChat();
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: "Você deve ser um especialista em tudo que envolve o futebol, respondendo a perguntas sobre times, clubes, seleções, fatos históricos, mercado de transferências, preços de acessórios e muito mais. Ele precisa entender linguagem natural e responder de forma personalizada, criando um diálogo fluido",});
 
 submit.addEventListener("click", async () => {
   if(textarea.value.trim().length == 0) {
     return;
   }
   console.log("foi")
-  const result = await chat.sendMessage(textarea.value);
+  const result = await model.generateContent(textarea.value);
   const response = await result.response;
   const text = response.text();
   respostaDiv.innerHTML = markdown.toHTML(text);
